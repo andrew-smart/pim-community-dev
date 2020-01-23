5857d61863a37f5d6b89e76d2821b0cfedda811d
Paul Chasle <paul.chasle@akeneo.com>
2020-01-23 18:07:03 +0100

    API-936: Add error boundary to connection settings

--- src/Akeneo/Connectivity/Connection/front/src/settings/pages/Index.tsx
+++ src/Akeneo/Connectivity/Connection/front/src/settings/pages/Index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import {ErrorBoundary} from '../../shared/ErrorBoundary';
 import {ConnectionsProvider} from '../connections-context';
 import {CreateConnection} from './CreateConnection';
 import {DeleteConnection} from './DeleteConnection';
@@ -9,28 +10,30 @@ import {RegenerateConnectionPassword} from './RegenerateConnectionPassword';
 import {RegenerateConnectionSecret} from './RegenerateConnectionSecret';
 
 export const Index = () => (
-    <ConnectionsProvider>
-        <Router>
-            <Switch>
-                <Route path='/connections/:code/edit'>
-                    <EditConnection />
-                </Route>
-                <Route path='/connections/:code/regenerate-secret'>
-                    <RegenerateConnectionSecret />
-                </Route>
-                <Route path='/connections/:code/regenerate-password'>
-                    <RegenerateConnectionPassword />
-                </Route>
-                <Route path='/connections/:code/delete'>
-                    <DeleteConnection />
-                </Route>
-                <Route path='/connections/create'>
-                    <CreateConnection />
-                </Route>
-                <Route path='/connections'>
-                    <ListConnections />
-                </Route>
-            </Switch>
-        </Router>
-    </ConnectionsProvider>
+    <ErrorBoundary>
+        <ConnectionsProvider>
+            <Router>
+                <Switch>
+                    <Route path='/connections/:code/edit'>
+                        <EditConnection />
+                    </Route>
+                    <Route path='/connections/:code/regenerate-secret'>
+                        <RegenerateConnectionSecret />
+                    </Route>
+                    <Route path='/connections/:code/regenerate-password'>
+                        <RegenerateConnectionPassword />
+                    </Route>
+                    <Route path='/connections/:code/delete'>
+                        <DeleteConnection />
+                    </Route>
+                    <Route path='/connections/create'>
+                        <CreateConnection />
+                    </Route>
+                    <Route path='/connections'>
+                        <ListConnections />
+                    </Route>
+                </Switch>
+            </Router>
+        </ConnectionsProvider>
+    </ErrorBoundary>
 );
